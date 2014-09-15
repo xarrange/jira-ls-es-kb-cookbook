ad0f4803ff26232f46a42f3ef1fab5c630147367
xarrange <x_arrange@mail.ru>
2014-09-15 16:03:49 +0300

    Starting cookbook

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+maintainer       "Leonid"
+maintainer_email "agoddard@mbl.edu"
+license           "Apache 2.0"
+description      "Installs/Configures Jira+Logstash+Elasticsearch+Kibana"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+depends           "java"
+depends           "jira"
+depends           "logstash"
+depends           "elasticsearch"
