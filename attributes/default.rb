9aa8d4befac6118990fa40475d4a9cc9239995eb
xarrange <x_arrange@mail.ru>
2014-09-17 15:21:18 +0000

    new

--- attributes/default.rb
+++ attributes/default.rb
@@ -28,11 +28,11 @@ default['logstash']['basedir'] = '/opt/logstash'
 default['logstash']['log_dir'] = '/opt/logstash/logs'
 default['logstash']['pid_dir'] = '/opt/logstash/run'
 default['logstash']['elasticsearch_cluster'] = 'logstash'
-default['logstash']['elasticsearch_ip'] = ''
-default['logstash']['elasticsearch_port'] = ''
+default['logstash']['elasticsearch_ip'] = '127.0.0.1'
+default['logstash']['elasticsearch_port'] = '9200'
 
 default['logstash']['agent']['inputs'] = [] 
 default['logstash']['agent']['outputs'] = ['tcp'=>['host=>\'172.30.0.235\'', 'port=>\'8000\'']]
 default['logstash']['server']['inputs'] = ['tcp'=>['port=>\'8000\'', 'type=>\'zz\'']]
-default['logstash']['server']['outputs'] = []
+default['logstash']['server']['outputs'] = ['elasticsearch'=>['host=>\'127.0.0.1\'', 'port=>\'9292\'']]
 
--- recipes/server.rb
+++ recipes/server.rb
@@ -1,3 +1,3 @@
 include_recipe 'logstash::server'
-include_recipe 'apache2::mod_php5'
-include_recipe 'logstash::kibana'
+include_recipe 'elasticsearch'
+include_recipe 'kibana'
